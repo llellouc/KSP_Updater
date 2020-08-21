1ec7c364a0b4e760249a33d6986cc823aa9c9c13
Leo <leo-lellouche@hotmail.fr>
2020-08-21 16:24:50 +0200

    v0.3.1 Display logs in columns (added, updated, up to date, failed)

---
+++ KSPUpdater/KSPUpdater.Client/UpdateDisplay/UpdateStatusToVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace KSPUpdater.Client.UpdateDisplay
+{
+    public class UpdateStatusToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object valueObj, Type targetType, object parameterObj, CultureInfo culture)
+        {
+            if(valueObj == null)
+                throw new ArgumentNullException(nameof(valueObj));
+            if(parameterObj == null)
+                throw new ArgumentNullException(nameof(parameterObj));
+
+            var value = (UpdateStatus) valueObj;
+            var parameter = Enum.Parse<UpdateStatus>((string) parameterObj);
+
+            if(value == parameter)
+                return Visibility.Visible;
+            //else
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
